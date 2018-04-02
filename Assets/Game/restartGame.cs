064cf557c44e4918e5fe4590566a38f7c6c79426
Ivo Linde <IvoIngusLinde@hotmail.com>
2018-04-02 18:40:53 +0300

    Added Restart button

---
+++ Assets/Game/restartGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class restartGame : MonoBehaviour {
+
+    public GameObject restartButton;
+
+    void Start()
+    {
+        Button btn = restartButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+}
