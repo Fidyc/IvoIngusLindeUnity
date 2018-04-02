a9ad9a9e9acd28bbf56c1012ccc9e17c315f3631
Ivo Linde <IvoIngusLinde@hotmail.com>
2018-04-02 19:24:43 +0300

    Added Death Sound

--- Assets/BasicBandit/deathCollision.cs
+++ Assets/BasicBandit/deathCollision.cs
@@ -6,15 +6,24 @@ public class deathCollision : MonoBehaviour {
 
     public GameObject Character;
     public GameObject GameOverText;
+    public GameObject deathSound;
+    public GameObject ambientSound;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.GetComponent<Collider>().tag == "deathObjects")
         {
-            Debug.Log("Object collided with deathObjects");
-            Character.GetComponent<Animator>().enabled = false;
-            GameOverText.GetComponent<Animation>().Play("GameOverAnim");
-
+            StartCoroutine(Death());
+            
         }
+    }
+    private IEnumerator Death()
+    {
+        Debug.Log("Object collided with deathObjects");
+        Character.GetComponent<Animator>().enabled = false;
+        deathSound.GetComponent<AudioSource>().Play();
+        yield return new WaitForSecondsRealtime(1);
+        ambientSound.GetComponent<AudioSource>().Stop();
+        GameOverText.GetComponent<Animation>().Play("GameOverAnim");
     }
 }
