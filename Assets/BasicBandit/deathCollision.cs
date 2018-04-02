ad8f4817b3d7788f0ba2c6ad29d935cb2e13568e
Ivo Linde <IvoIngusLinde@hotmail.com>
2018-04-02 15:15:18 +0300

    Added Trap Killer, Ragdoll, Collision trigger for Ragdoll
    deathObjects tag - When objects tagged with "deathObjects" collide with player char, char animator is diabled, and ragdoll starts function

---
+++ Assets/BasicBandit/deathCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathCollision : MonoBehaviour {
+
+    public GameObject Character;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Collider>().tag == "deathObjects")
+        {
+            Debug.Log("Object collided with deathObjects");
+            Character.GetComponent<Animator>().enabled = false;
+        }
+    }
+}
--- Assets/Traps/TrapDoor/TrapTrigger.cs
+++ Assets/Traps/TrapDoor/TrapTrigger.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class TrapTrigger : MonoBehaviour {
 
     public GameObject TrapDoor;
+    public GameObject DeathSphere;
 
     private void OnTriggerEnter(Collider other)
     {
         TrapDoor.GetComponent<Animation>().Play("TrapAnim");
+        DeathSphere.GetComponent<Rigidbody>().useGravity = true;
     }
 }
