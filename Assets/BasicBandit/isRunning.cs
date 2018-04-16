ddb6a9d4a990767d9c43e22eb4e0ea15ea5bdd5c
Ivo Linde <IvoIngusLinde@hotmail.com>
2018-04-17 00:04:49 +0300

    Added food system
    Known issues:
    Sometimes animation gets stuck
    No display for food counter

--- Assets/BasicBandit/isRunning.cs
+++ Assets/BasicBandit/isRunning.cs
@@ -6,19 +6,22 @@ public class isRunning : MonoBehaviour {
 
 	private Animator Animator;
 	private CharacterController controller;
-
 	public float speed = 0.2f;
+    public int food = 5;
 	public float turnSpeed = 60.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	public float gravity = 20.0f;
+    public GameObject poop;
+    public GameObject Character;
+    private bool beingHandled = false;
 
-	void Start () {
+    void Start () {
 		controller = GetComponent <CharacterController>();
 		Animator = gameObject.GetComponentInChildren<Animator>();
 	}
 
 	void Update (){
-		if (Input.GetKey ("up") || Input.GetKey("w") || Input.GetKey("down") || Input.GetKey("s"))
+		if (Input.anyKey)
         {
             if (Input.GetKey("up") || Input.GetKey("w"))
             {
@@ -33,6 +36,24 @@ public class isRunning : MonoBehaviour {
             {
                 Animator.SetInteger("isRunning", 3);
             }
+            if (Input.GetKeyDown("e"))
+            {
+                Animator.SetInteger("isRunning", 4);
+                GameObject closest;
+                closest = FindClosestPizza();
+                if (closest != null)
+                {
+                    food += 1;
+                    Debug.Log("Food " + food);
+                    Destroy(closest);
+                }
+            }
+            if (Input.GetKeyDown("x") && food > 0 && !beingHandled)
+            {
+                Animator.SetInteger("isRunning", 5);
+                food -= 1;
+                StartCoroutine(Sleep());
+            }
         }  else {
 			Animator.SetInteger ("isRunning", 0);
 		}
@@ -45,5 +66,58 @@ public class isRunning : MonoBehaviour {
 		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 		controller.Move(moveDirection * Time.deltaTime);
 		moveDirection.y -= gravity * Time.deltaTime;
-	}
+        
+    }
+
+    public GameObject FindClosestPizza()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("pizzaTag");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        if (distance < 5)
+        {
+            Debug.Log("distance " + distance);
+            return closest;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    /*public IEnumerator Poop()
+    {
+        GameObject PoopDuplicate;
+        Vector3 pos = Character.transform.position;
+        pos = pos + new Vector3(0, 0, -0.5f);
+        Quaternion rot = Character.transform.rotation;
+        Sleep(500);
+        PoopDuplicate = Instantiate(poop, pos, rot);
+        PoopDuplicate.SetActive(true);
+    }*/
+    private IEnumerator Sleep()
+    {
+        beingHandled = true;
+        // process pre-yield
+        GameObject PoopDuplicate;
+        Vector3 pos = Character.transform.position;
+        pos = pos + new Vector3(0, 0, -0.6f);
+        Quaternion rot = Character.transform.rotation;
+        yield return new WaitForSeconds(1.0f);
+        PoopDuplicate = Instantiate(poop, pos, rot);
+        PoopDuplicate.SetActive(true);
+        // process post-yield
+        beingHandled = false;
+    }
 }
